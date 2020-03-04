cb046d5e0b512e7840a802995b82667fa2feea51
CreeJee <000as00060@gmail.com>
2020-03-05 03:36:26 +0900

    component base create added

--- hook/reconcile.js
+++ hook/reconcile.js
@@ -4,10 +4,20 @@ class ReconcileGenError extends Error {
         super(msg);
     }
 }
-
-const noOpUpdate = (current) => {};
+const onUpdate = (node, current, key) => {
+    switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            node.setAttribute(key, current);
+            break;
+        case Node.TEXT_NODE:
+            node.nodeValue = current;
+            break;
+        default:
+            throw new ReconcileGenError("unaccepted data");
+    }
+};
 const noOpCond = (current, before) => true;
-const updater = (old, view, isUpdate = noOpCond, onUpdate = noOpUpdate) => {
+const updater = (old, view, isUpdate = noOpCond) => {
     //needs bound self
     return function __nestedUpdate__(item) {
         const collector = view.collect(this);
@@ -15,19 +25,19 @@ const updater = (old, view, isUpdate = noOpCond, onUpdate = noOpUpdate) => {
             const current = item[key];
             const before = old[key];
             if (current !== before && isUpdate(current, before)) {
-                onUpdate(current);
+                onUpdate(collector[key], current, key);
             }
         }
     };
 };
-const createItem = (item, itemGroup) => {
+const createItem = (item, itemGroup, isUpdate, onUpdate) => {
     const root = itemGroup.cloneNode(true);
     let rootChild = root.firstChild;
     let itemChild = itemGroup.firstChild;
     if (rootChild !== null && itemChild !== null) {
         do {
-            rootChild.update = updater(item, itemChild);
-            updater({}, itemChild).call(rootChild, item);
+            rootChild.update = updater(item, itemChild, isUpdate);
+            updater({}, itemChild, isUpdate).call(rootChild, item);
         } while (
             (rootChild = rootChild.nextSibling) &&
             (itemChild = itemChild.nextSibling)
@@ -35,10 +45,10 @@ const createItem = (item, itemGroup) => {
     }
     return root;
 };
-export const create = (view, data, child) => {
-    if (!Array.isArray(data)) {
-        throw new ReconcileGenError("data is must Array!");
-    }
+// child는 function hook함수 자체를 재정의 하지않는다는게 컨밴션임 ㅇㅇ
+// 그에따라 관련값은 caller에 저장하고 로직을 진행함
+
+export const create = (view, component) => {
     const { ref } = view.collect(view);
     let renderedItems = [];
 
@@ -47,32 +57,14 @@ export const create = (view, data, child) => {
             ref,
             renderedItems,
             data,
-            (item) => createItem(item, child),
+            (item) => {
+                const view = component();
+                return createItem(item, view);
+            },
             (node, item) => node.update(item)
         );
         renderedItems = data.slice();
     };
-    view.update(data);
     return view;
 };
-// 유사 hook ㄱ?
-const hookTable = new Map();
-export function useReconclie(self, onCycle) {
-    let hookRef = [];
-    const effect = () => {
-        const unMount = onCycle();
-        const unMountEffect = () => {
-            unMount();
-        };
-        hookRef.push(unMountEffect);
-        hookRef.splice(hookRef.indexOf(onCycle), 1);
-    };
-    if (hookTable.has(self)) {
-        hookRef = hookTable.get(self);
-    } else {
-        hookTable.set(self, hookRef);
-    }
-    hookRef.push(effect);
-}
-
 export default create;
--- index.js
+++ index.js
@@ -1,115 +1,130 @@
 class Ref {
-  constructor(idx, ref) {
-    this.idx = idx;
-    this.ref = ref;
-  }
+    constructor(idx, ref) {
+        this.idx = idx;
+        this.ref = ref;
+    }
 }
 
-const TREE_WALKER = document.createTreeWalker(document, NodeFilter.SHOW_ALL, null, false);
-const compilerTemplate = document.createElement('template');
+const TREE_WALKER = document.createTreeWalker(
+    document,
+    NodeFilter.SHOW_ALL,
+    null,
+    false
+);
+const compilerTemplate = document.createElement("template");
 const collector = (node) => {
-  if (node.nodeType !== 3) {
-    if (node.attributes !== undefined) {
-      for(let attr of Array.from(node.attributes)) {
-        let aname = attr.name
-        if (aname[0] === '#') {
-          node.removeAttribute(aname)
-          return aname.slice(1)
+    if (node.nodeType !== 3) {
+        if (node.attributes !== undefined) {
+            for (let attr of Array.from(node.attributes)) {
+                let aname = attr.name;
+                if (aname[0] === "#") {
+                    let rName = aname.slice(1);
+                    node.removeAttribute(aname);
+                    node.setAttribute(rName, "");
+                    return aname.slice(1);
+                }
+            }
         }
-      }
-    }
-    return 0
-  } else {
-    let nodeData = node.nodeValue
-    if (nodeData[0] === '#') {
-      node.nodeValue = ""
-      return nodeData.slice(1)
-    }
-    return 0
-  }
-}
-const genPath = (node) => {
-  const w = TREE_WALKER
-  w.currentNode = node
-
-  let indices = [], ref, idx = 0
-  do {
-    if (ref = collector(node)) {
-      indices.push(new Ref(idx+1, ref))
-      idx = 1
+        return 0;
     } else {
-      idx++
+        let nodeData = node.nodeValue;
+        if (nodeData[0] === "#") {
+            node.nodeValue = "";
+            return nodeData.slice(1);
+        }
+        return 0;
     }
-  } while(node = w.nextNode())
+};
+const genPath = (node) => {
+    const w = TREE_WALKER;
+    w.currentNode = node;
 
-  return indices
-}
+    let indices = [],
+        ref,
+        idx = 0;
+    do {
+        if ((ref = collector(node))) {
+            indices.push(new Ref(idx + 1, ref));
+            idx = 1;
+        } else {
+            idx++;
+        }
+    } while ((node = w.nextNode()));
+
+    return indices;
+};
 
 TREE_WALKER.roll = function(n) {
-  while(--n) this.nextNode()
-  return this.currentNode
-}
-
+    while (--n) this.nextNode();
+    return this.currentNode;
+};
 
 function walker(node) {
-  const refs = {}
-  const w = TREE_WALKER
+    const refs = {};
+    const w = TREE_WALKER;
 
-  w.currentNode = node;
-  this._refPaths.map(x => refs[x.ref] = w.roll(x.idx));  
-  return refs;
+    w.currentNode = node;
+    for (const x of this._refPaths) {
+        const ref = x.ref;
+        let rolled = w.roll(x.idx);
+        refs[ref] = rolled;
+    }
+    // this._refPaths.map((x) => (refs[x.ref] = w.roll(x.idx)));
+    return refs;
 }
-export const extractFragment = (strings, ...args) =>{
-  const template = String.raw(strings, ...args)
-    .replace(/>\n+/g, '>')
-    .replace(/\s+</g, '<')
-    .replace(/>\s+/g, '>')
-    .replace(/\n\s+/g, '<!-- -->')
-  compilerTemplate.innerHTML = template;
-  return compilerTemplate.content;
-}
+export const extractFragment = (strings, ...args) => {
+    const template = String.raw(strings, ...args)
+        .replace(/>\n+/g, ">")
+        .replace(/\s+</g, "<")
+        .replace(/>\s+/g, ">")
+        .replace(/\n\s+/g, "<!-- -->");
+    compilerTemplate.innerHTML = template;
+    return compilerTemplate.content;
+};
 export const compile = (node) => {
-    node._refPaths = genPath(node)
-    node.collect = walker
-}
+    node._refPaths = genPath(node);
+    node.collect = walker;
+};
 export const h = (strings, ...args) => {
-  const content = extractFragment(strings, ...args).firstChild
-  compile(content)
-  return content;
-}
+    const content = extractFragment(strings, ...args).firstChild;
+    compile(content);
+    return content;
+};
 //Fragment
 const getSelfPath = (node) => {
-  const w = TREE_WALKER
-  const parent = node.parentNode;
-  const siblings = parent !== null ? Array.from(parent.childNodes) : [];
-  
-  let current = node;
-  w.currentNode = node
+    const w = TREE_WALKER;
+    const parent = node.parentNode;
+    const siblings = parent !== null ? Array.from(parent.childNodes) : [];
 
-  let indices = [], ref, idx = 0
-  do {
-    if (ref = collector(current)) {
-      indices.push(new Ref(idx+1, ref))
-      idx = 1
-    } else {
-      idx++
-    }
-    if (node !== current && siblings.includes(current)) {
-      break;
-    }
-  } while(current = w.nextNode())
+    let current = node;
+    w.currentNode = node;
 
-  return indices
-}
+    let indices = [],
+        ref,
+        idx = 0;
+    do {
+        if ((ref = collector(current))) {
+            indices.push(new Ref(idx + 1, ref));
+            idx = 1;
+        } else {
+            idx++;
+        }
+        if (!node.isSameNode(current) && siblings.includes(current)) {
+            break;
+        }
+    } while ((current = w.nextNode()));
+
+    return indices;
+};
 export const fragmentCompile = (node) => {
-  for (const self of node.childNodes) {
-    self._refPaths = getSelfPath(self);
-    self.collect = walker
-  }
-}
+    for (const self of node.childNodes) {
+        self._refPaths = getSelfPath(self);
+        self.collect = walker;
+    }
+};
 export const fragment = (strings, ...args) => {
-  const content = extractFragment(strings, ...args)
-  fragmentCompile(content);
-  return content;
-}
+    const content = extractFragment(strings, ...args);
+    fragmentCompile(content);
+    return content;
+};
 export default h;
